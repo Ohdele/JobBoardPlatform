61155af7ce2c5f6d74ae5e31b5850f6cf85553c9
Dele <adesinaayodele48@gmail.com>
2024-08-08 21:53:14 +0100

    Remove duplicated files from the root directory

--- backend/test_imports.py
+++
@@ -1,9 +0,0 @@
-try:
-    from flask import Flask
-    from flask_sqlalchemy import SQLAlchemy
-    from flask_jwt_extended import JWTManager
-    from backend.config import Config  # Adjust path if needed
-    from backend.auth import auth  # Adjust path if needed
-    print("All imports are successful.")
-except ImportError as e:
-    print(f"ImportError: {e}")
